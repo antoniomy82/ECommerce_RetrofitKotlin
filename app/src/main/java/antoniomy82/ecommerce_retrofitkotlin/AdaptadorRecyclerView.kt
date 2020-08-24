1a2bdf7e9bc01c78740a6a63b16bdf3b811282f5
Antonio <antoniomy82@gmail.com>
2020-08-24 17:29:32 +0200

    Agregamos a recyclerview_item_lista.xml Categoria y retoco los margenes de la propiedad en el AdaptadorRecyclerView.kt, en concreto en LayoutInflater

--- app/src/main/java/antoniomy82/ecommerce_retrofitkotlin/AdaptadorRecyclerView.kt
+++ app/src/main/java/antoniomy82/ecommerce_retrofitkotlin/AdaptadorRecyclerView.kt
@@ -1,6 +1,7 @@
 package antoniomy82.ecommerce_retrofitkotlin
 
 import android.content.Context
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,14 +31,14 @@ class AdaptadorRecyclerView(var context: Context, listaItems: ArrayList<Ecommerc
     //Aquí es dónde vamos a crear o inflar la vista
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        var miContentView: View? = null
+        lateinit var miContentView: View
 
-        miContentView = LayoutInflater.from(context).inflate(R.layout.recyclerview_item_lista, null) //Aquí cambiamos el tipo de vista item_grid o Item_lista
+        miContentView = LayoutInflater.from(context).inflate(R.layout.recyclerview_item_lista, parent, false) //Aquí cambiamos el tipo de vista item_grid o Item_lista
 
         println("Create View Holder: $viewType")
 
         //Barra de inicio
-        return ViewHolder(miContentView!!) //Devolvemos el ViewHolder que hemos creado debajo, con la View que acabamos de inflar
+        return ViewHolder(miContentView) //Devolvemos el ViewHolder que hemos creado debajo, con la View que acabamos de inflar
     }
 
     //Asignamos los datos a cada elemento de la lista
@@ -45,15 +46,22 @@ class AdaptadorRecyclerView(var context: Context, listaItems: ArrayList<Ecommerc
         val ecommerce = listaCopia!![position] // Cargamos los elementos
         val miViewHolder = holder as ViewHolder?
 
-        val direccion: String = ecommerce.address!!.street.toString() + " , " + ecommerce.address!!.city + " , ("+ ecommerce.address!!.country+")"
+        val direccion: String = ecommerce.address!!.street + " , " + ecommerce.address!!.city + " , ("+ ecommerce.address!!.country+")"
         val distancia: String = "Distancia: "+String.format("%.2f",(ecommerce.distance)?.div(1000)) + " Km"  //Convierto a KM con 2 decimales
-
+        val cateoria : String = "Categoria: "+ ecommerce.category
         if (miViewHolder != null) {
             miViewHolder.tvNombre.text = ecommerce.name
             miViewHolder.tvDireccion.text = direccion
             miViewHolder.tvDistancia.text = distancia
+            miViewHolder.tvCategoria.text = cateoria
         }
 
+        //Pongo las celdas pares de otro color
+        if (position % 2 == 0) {
+            miViewHolder!!.itemView.setBackgroundColor(Color.parseColor("#eeeeee")) //android:background="#FFFFFF"
+        } else {
+            miViewHolder!!.itemView.setBackgroundColor(Color.parseColor("#FFFFFF")) //"#e6e2d3"
+        }
 
 
         println("Bind View Holder: $position")
@@ -69,7 +77,7 @@ class AdaptadorRecyclerView(var context: Context, listaItems: ArrayList<Ecommerc
     }
 
     override fun getItemCount(): Int {
-        return listaCopia!!.size!!
+        return listaCopia!!.size
     }
 
 
@@ -79,6 +87,7 @@ class AdaptadorRecyclerView(var context: Context, listaItems: ArrayList<Ecommerc
         var tvNombre: TextView = itemView.findViewById(R.id.tvNombre)
         var tvDireccion : TextView = itemView.findViewById(R.id.tvDireccion)//ListView
         var tvDistancia : TextView = itemView.findViewById(R.id.tvDistancia)
+        var tvCategoria : TextView = itemView.findViewById(R.id.tvCategoria)
 
     }
 
@@ -92,15 +101,15 @@ class AdaptadorRecyclerView(var context: Context, listaItems: ArrayList<Ecommerc
         override fun performFiltering(constraint: CharSequence): FilterResults {
             val filteredList: MutableList<Ecommerce> =
                 ArrayList<Ecommerce>()
-            if (constraint == null || constraint.isEmpty()) {
+            if (constraint.isEmpty()) {
                 listaFull?.let { filteredList.addAll(it) }
             } else {
-                val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
+                val filterPattern = constraint.toString().toLowerCase(Locale.ROOT).trim { it <= ' ' }
 
                 Log.d("ListaItems", listaFull?.size.toString())
 
                 for (item in listaFull!!) {
-                    if (item.name?.toLowerCase()?.contains(filterPattern)!!) {
+                    if (item.name?.toLowerCase(Locale.ROOT)?.contains(filterPattern)!!) {
                         Log.d("Item->", item.name!!)
                         filteredList.add(item)
                     }
@@ -114,7 +123,7 @@ class AdaptadorRecyclerView(var context: Context, listaItems: ArrayList<Ecommerc
         override fun publishResults(constraint: CharSequence, results: FilterResults) {
             //MainActivity.setEsBusqueda(listaFull) //Activamos un booleano para saber que tipo de carga hay que hacer y hacemos una copia de la lista de contactos
             listaCopia?.clear()
-            listaCopia?.addAll((results.values as ArrayList<Ecommerce>))
+            listaCopia?.addAll(results.values as ArrayList<Ecommerce>)
             notifyDataSetChanged()
         }
     }
