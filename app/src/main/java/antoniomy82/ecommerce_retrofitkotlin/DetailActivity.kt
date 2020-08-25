cd90633307a2e12f4964e41e1b5b3e6a9e0ec55d
Antonio <antoniomy82@gmail.com>
2020-08-25 03:19:52 +0200

    Añado lanzar Action DIAL (llamada telefónica) y enviar un email

--- app/src/main/java/antoniomy82/ecommerce_retrofitkotlin/DetailActivity.kt
+++ app/src/main/java/antoniomy82/ecommerce_retrofitkotlin/DetailActivity.kt
@@ -1,6 +1,7 @@
 
 package antoniomy82.ecommerce_retrofitkotlin
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -23,7 +24,7 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         //Definimos los enlaces con activity_detail
-        var im_Dlogo : ImageView? =findViewById(R.id.imLogo)
+        val im_Dlogo : ImageView? =findViewById(R.id.imLogo)
         val tv_Dnombre: TextView? =findViewById(R.id.tv_Dnombre)
         val tv_Dcategoria: TextView? =findViewById(R.id.tv_Dcategoria)
         val tv_Ddireccion: TextView? =findViewById(R.id.tv_Ddireccion)
@@ -35,20 +36,49 @@ class DetailActivity : AppCompatActivity() {
         miEcommerce=MainActivity.getEcommerce(intent.getIntExtra("miIndice",0))
         val direccion:String =miEcommerce!!.address!!.street+ ", " + miEcommerce!!.address!!.zip+ " , "+miEcommerce!!.address!!.city+ ","+miEcommerce!!.address!!.country
 
-        //im_Dlogo!!.setImageResource(miEcommerce!!.logo!!.url)
         tv_Dnombre!!.text=miEcommerce!!.name
         tv_Dcategoria!!.text=miEcommerce!!.category
         tv_Ddireccion!!.text=direccion
         tv_Demail!!.text=miEcommerce!!.contact!!.email
         tv_Dtelefono!!.text=miEcommerce!!.contact!!.phone
 
-
+        //Cargo el logo almacenado con Picasso
         if((miEcommerce?.logo?.url)!=null) {
             Picasso.get().load(miEcommerce!!.logo!!.url).placeholder(R.drawable.nologo).into(im_Dlogo)
             Log.d("Dentro Picasso", "Se pasa por el carajo isNotEmpty")
         }
         else{
             Picasso.get().load(R.drawable.noimage).into(im_Dlogo)
+        }
+
+
+        //Cuando se haga click la imagen de mail, abrirá un cliente de correo  con la dirección que tenemos guardada
+        // Nota: (declarar intent filter en Manifest), si no tenemos dirección de mail, la dejará en blanco.
+         findViewById<View>(R.id.imEmail).setOnClickListener {
+             val miEmail = arrayOf<String>(miEcommerce!!.contact!!.email) //Tengo que hacer este cast raro si o si, sino no me aparece email
+             val intent = Intent(Intent.ACTION_SENDTO).apply {
+                   data = Uri.parse("mailto:") // only email apps should handle this
+                   putExtra(Intent.EXTRA_EMAIL,miEmail)
+                   putExtra(Intent.EXTRA_SUBJECT,"eCommerce -->Contráteme ;-) ")
+                }
+
+             if (intent.resolveActivity(packageManager) != null) {
+                   startActivity(intent)
+             }
+         }
+
+        //Cuando hagamos click sobre el icono del telefono, llamaremos
+        findViewById<View>(R.id.imTelefono).setOnClickListener {
+            if (miEcommerce?.contact?.phone !="" || miEcommerce?.contact?.phone !=null) {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:" + miEcommerce?.contact?.phone)
+
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+            } else {
+                Toast.makeText(applicationContext, "No hay teléfono en ese campo o su formato es erroneo", Toast.LENGTH_LONG).show()
+            }
         }
 
         findViewById<View>(R.id.imMaps).setOnClickListener {
@@ -94,4 +124,5 @@ class DetailActivity : AppCompatActivity() {
         }
 
     }
+
 }
